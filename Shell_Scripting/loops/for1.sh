6f45297626a899a5a74768b1a1a701ceb04d3a1e
bytebridgesalone <bytebridgesalone@gmail.com>
2024-03-07 12:06:43 +0000

    for loop

---
+++ Shell_Scripting/loops/for1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# We use for loops to iterate over a set of elements or values.
+for i in 1 2 3 4 5
+do
+	echo "Looping ... number $i"
+done
---
+++ Shell_Scripting/loops/for2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Looping through 10 values
+
+# VALUE=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# VALUE=10
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+	echo "Looping ... number $i"
+done
