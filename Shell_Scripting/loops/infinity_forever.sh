dfced671d4af73555602fd1a83a6107f0229d0ec
bytebridgesalone <bytebridgesalone@gmail.com>
2024-03-08 21:30:54 +0000

    best school

---
+++ Shell_Scripting/loops/infinity_forever.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# An Infinite loop that prints "ByteBridge Forever"
+
+while true; do
+	echo "ByteBridge Salone forever!"
+	echo "Best Computer School in Mama Salone"
+	sleep 2
+done
